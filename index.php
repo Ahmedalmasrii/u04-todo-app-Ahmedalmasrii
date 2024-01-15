ded2fd21559d6c412344224c1fa28c54f61dacf9
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-01-15 15:59:07 +0000

    utför test på index html

--- index.php
+++ index.php
@@ -2,11 +2,27 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
     <title>Document</title>
 </head>
+
 <body>
+    <div class="container">
+        <div class="todo-app">
+            <h2>To-do list <img src="/images/clipboard-list-solid.svg" alt="icon"></h2>
+
+            <form action="welcome.php" method="post">
+                Name: <input type="text" name="name"><br>
+                E-mail: <input type="text" name="email"><br>
+                <input type="submit">
+            </form>
+        </div>
+
+    </div>
 </body>
+
 </html>
