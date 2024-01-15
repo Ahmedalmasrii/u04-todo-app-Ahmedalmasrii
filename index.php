edc68dc58ef0cfa6dbef7efdcc84a412e38568a1
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-01-15 18:51:54 +0000

    funktioner för att kontrollera använarens behörigheter

--- index.php
+++ index.php
@@ -16,3 +16,26 @@ function registerUser($conn, $name, $email, $password)
     $stmt->bind_param("sss", $name, $email, $hashedPassword);
     $stmt->execute();
 }
+
+// Funktion för att kontrollera och logga in en användare
+function loginUser($conn, $email, $password)
+{
+    // Förbereder ett SQL-uttalande för att hämta användar-ID och krypterat lösenord baserat på e-post som har matats in vid skapandet
+    $stmt = $conn->prepare("SELECT id, password FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+
+    // Kontrollerar om  användare med den angivna e-postadressen finns i databasen
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($userId, $hashedPassword);
+        $stmt->fetch();
+
+        // Verifierar det angivna lösenordet mot det krypterade lösenordet
+        if (password_verify($password, $hashedPassword)) {
+            return $userId; // Returnerar användar-ID om inloggningen lyckas
+        }
+    }
+
+    return false; // Returnerar false om inloggningen misslyckas
+}
