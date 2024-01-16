a7d492bb58ef6d145a59a9f44cd018e5b804ccfa
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-01-16 18:41:07 +0000

    Funktion för att markera att uppgiften är klar annars visa felmeddelande

--- uppgiftsidan.php
+++ uppgiftsidan.php
@@ -17,3 +17,19 @@ function addTask($conn, $task)
         echo "<div class='error-message'>$errorMessage</div>";
     }
 }
+
+// Funktion för att markera en uppgift som klar
+function completeTask($conn, $id)
+{
+    $stmt = $conn->prepare("UPDATE todo SET klar = 1 WHERE ID = ?");
+    $stmt->bind_param("i", $id);
+
+    // Kolla om uppgiften har markerats som klar, annars visa ett felmeddelande
+    if ($stmt->execute()) {
+        $message = "Bra jobbat! Uppgiften är nu klar!";
+        echo "<div class='success-message'>$message</div>";
+    } else {
+        $errorMessage = "Hoppsan! Något gick fel när du försökte markera uppgiften som klar: " . $stmt->error;
+        echo "<div class='error-message'>$errorMessage</div>";
+    }
+}
