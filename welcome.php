5e7fd120a352603542f3821a875f9f51c7f852eb
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-01-16 18:29:34 +0000

    lagt till en html struktur hur sidan ska se ut

--- welcome.php
+++ welcome.php
@@ -43,3 +43,26 @@ if (isset($_SESSION['userId'])) {
 $conn->close();
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>Välkomstsida</title>
+</head>
+
+<body>
+    <!-- Visar välkomstmeddelande och användarinformation namn och email  på startsidan vid inloggning-->
+    <h1>Välkommen <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?></h1>
+    <p>Din e-postadress är: <?php echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8'); ?></p>
+
+    <!-- Länk till en annan sida (uppgiftsidan.php) -->
+    <a href="uppgiftsidan.php">
+        <button>Klicka här för att se alla uppgifter</button>
+    </a>
+</body>
+
+</html>
