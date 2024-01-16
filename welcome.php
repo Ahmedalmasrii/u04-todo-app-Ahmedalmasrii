d10c719439b7f7ca7a099258e6ba277afebd956c
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-01-16 18:30:30 +0000

    lagt till en button för att länka till en annan sida

--- welcome.php
+++ welcome.php
@@ -57,7 +57,6 @@ $conn->close();
     <!-- Visar välkomstmeddelande och användarinformation namn och email  på startsidan vid inloggning-->
     <h1>Välkommen <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?></h1>
     <p>Din e-postadress är: <?php echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8'); ?></p>
-
     <!-- Länk till en annan sida (uppgiftsidan.php) -->
     <a href="uppgiftsidan.php">
         <button>Klicka här för att se alla uppgifter</button>
