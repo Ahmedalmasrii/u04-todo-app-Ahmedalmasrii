a0282b37fae993c9cbb013db486c1b598ea72d70
Ahmedalmasrii <ahmedalmasri2013@gmail.com>
2024-03-16 13:34:22 +0000

    rättat till min kod från mysqli till pdo

--- connect.php
+++ connect.php
@@ -1,12 +1,20 @@
 <?php
-$host = 'db';  // Detta bör matcha tjänstens namn som definierats i min Docker Compose-fil.
+$host = 'db';  // Anpassa dessa värden efter din miljö.
 $database = 'mariadb';
 $user = 'mariadb';
 $password = 'mariadb';
+$charset = 'utf8mb4';
 
-$conn = new mysqli($host, $user, $password, $database);
+$dsn = "mysql:host=$host;dbname=$database;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
